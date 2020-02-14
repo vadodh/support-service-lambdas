369e929a3bdf61a17fce50d6d650aff6d77a4b67
Francis Rhys-Jones <francis@rhys-jones.com>
2020-02-14 16:05:13 +0000

    migrating to use HttpUrlConnection rather than AsyncHttpClient

--- handlers/digital-voucher-api/src/main/scala/com/gu/digital_voucher_api/DigitalVoucherApiApp.scala
+++ handlers/digital-voucher-api/src/main/scala/com/gu/digital_voucher_api/DigitalVoucherApiApp.scala
@@ -1,16 +1,17 @@
 package com.gu.digital_voucher_api
 
+import cats.arrow.FunctionK
 import cats.data.EitherT
 import cats.implicits._
 import cats.effect.{ContextShift, IO}
 import com.gu.AppIdentity
 import com.gu.digital_voucher_api.imovo.ImovoClient
-import com.softwaremill.sttp.SttpBackend
-import com.softwaremill.sttp.asynchttpclient.cats.AsyncHttpClientCatsBackend
+import com.softwaremill.sttp.{HttpURLConnectionBackend, Id, SttpBackend}
 import com.typesafe.scalalogging.LazyLogging
 import org.http4s.HttpRoutes
 import org.http4s.server.middleware.Logger
 import org.http4s.util.CaseInsensitiveString
+import com.softwaremill.sttp.impl.cats.implicits._
 
 final case class DigitalVoucherApiError(message: String)
 
@@ -19,7 +20,10 @@ object DigitalVoucherApiApp extends LazyLogging {
   private implicit val cs: ContextShift[IO] = IO.contextShift(scala.concurrent.ExecutionContext.global)
 
   def apply(appIdentity: AppIdentity): EitherT[IO, DigitalVoucherApiError, HttpRoutes[IO]] = {
-    DigitalVoucherApiApp(appIdentity, AsyncHttpClientCatsBackend[cats.effect.IO]())
+    val catsHttpURLConnectionBackend =
+      sttpBackendToCatsMappableSttpBackend[Id, Nothing](HttpURLConnectionBackend())
+        .mapK(FunctionK.lift[Id, IO](IO.delay))
+    DigitalVoucherApiApp(appIdentity, catsHttpURLConnectionBackend)
   }
 
   def apply[S](appIdentity: AppIdentity, backend: SttpBackend[IO, S]): EitherT[IO, DigitalVoucherApiError, HttpRoutes[IO]] = {
